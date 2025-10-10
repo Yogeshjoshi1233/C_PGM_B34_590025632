98cb5f01ac9c90853c983b156bc49d50e1571b98
Yogeshjoshi1233 <yogeshjoshi001234@gmail.com>
2025-10-10 17:32:56 +0530

    Add files via upload

---
+++ power .c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+main() {
+int base, exponent;
+long long result = 1; 
+
+printf("Enter base number: ");
+scanf("%d", &base);
+
+printf("Enter exponent: ");
+scanf("%d", &exponent);
+
+for (int i = 1; i <= exponent; i++) {
+result = result * base; 
+}
+
+printf("%d raised to the power %d is %lld\n", base, exponent, result);
+
+return 0;
+}
